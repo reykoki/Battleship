3a55a6d6938e492332e4693a5b76129c2ad6303c
Daniel Torres <dato3600@colorado.edu>
2021-02-12 10:10:07 -0700

    Add files via upload
    added get and set methods for ship class (assumed it was to get and set name). Also added and formatted comments but feel free to change it

--- src/edu/colorado/TheQuintet/Main.py
+++ src/edu/colorado/TheQuintet/Main.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 """ generated source for module Main """
 # package: edu.colorado.csci44485448
+
+from Ship import Ship
+
 class Main(object):
-    """ generated source for class Main """
+    """
+    Generated source for class Main
+    """
     @classmethod
-    def main(cls, args):
-        """ generated source for method main """
-        #  write your code here
+    def main(cls):
+        """
+        Generated source for method main
+        """
         #  Github bout to be so unhappy with us - Pair 2
         ship = Ship()
         ship.show()
 
-
 if __name__ == '__main__':
-    import sys
-    Main.main(sys.argv)
+    m = Main()
+    m.main()
 
--- src/edu/colorado/TheQuintet/Ship.py
+++ src/edu/colorado/TheQuintet/Ship.py
@@ -4,8 +4,13 @@
 #  This is the  baseclass for your ship.  Modify accordingly
 #  TODO: practice good OO design
 class Ship(object):
-    """ generated source for class Ship """
+    """
+    Generated source for class Ship
+    """
     name = str()
+
+    # def __init__(self, name):
+    #     self.setName(name)
 
     #  Team TheQuintet, pair 1 was here. 
     #  Team Quintent, pair 2 was here
@@ -13,7 +18,20 @@ class Ship(object):
     #  TODO: Understand encapsulation
     #  TODO: Understand what these todo comments mean
     def show(self):
-        """ generated source for method show """
+        """
+        Generated source for method show
+        """
         #  dunno why this is here maybe it is just an example method
-        print "IF you can't see this then something is severely wrong!!"
+        print("IF you can't see this then something is severely wrong!!")
 
+    def setName(self, ship_name):
+        """
+        Set ship name
+        """
+        self.name = ship_name
+
+    def getName(self):
+        """
+        Get ship name
+        """
+        return self.name
