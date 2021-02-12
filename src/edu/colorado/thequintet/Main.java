d0a7b320a6dea14e0abe5ed9f8289c0c85d53a82
reycronin <reykokimonster@gmail.com>
2021-02-12 13:35:36 -0700

    lowercase

---
+++ src/edu/colorado/thequintet/Main.java
@@ -0,0 +1,12 @@
+package edu.colorado.thequintet;
+  
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        // Github bout to be so unhappy with us - Pair 2
+         Ship ship = new Ship();
+         ship.show();
+
+    }
+}
---
+++ src/edu/colorado/thequintet/Main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+""" generated source for module Main """
+# package: edu.colorado.csci44485448
+
+from Ship import Ship
+
+class Main(object):
+    """
+    Generated source for class Main
+    """
+    @classmethod
+    def main(cls):
+        """
+        Generated source for method main
+        """
+        #  Github bout to be so unhappy with us - Pair 2
+        ship = Ship()
+        ship.show()
+
+if __name__ == '__main__':
+    m = Main()
+    m.main()
+
