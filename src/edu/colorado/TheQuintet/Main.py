bb99a88e7d51559f23a5601a38af2c2d657a1e96
reycronin <reykokimonster@gmail.com>
2021-02-11 17:26:22 -0700

    java2python

---
+++ src/edu/colorado/TheQuintet/Main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+""" generated source for module Main """
+# package: edu.colorado.csci44485448
+class Main(object):
+    """ generated source for class Main """
+    @classmethod
+    def main(cls, args):
+        """ generated source for method main """
+        #  write your code here
+        #  Github bout to be so unhappy with us - Pair 2
+        ship = Ship()
+        ship.show()
+
+
+if __name__ == '__main__':
+    import sys
+    Main.main(sys.argv)
+
---
+++ src/edu/colorado/TheQuintet/Ship.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+""" generated source for module Ship """
+# package: edu.colorado.csci44485448
+#  This is the  baseclass for your ship.  Modify accordingly
+#  TODO: practice good OO design
+class Ship(object):
+    """ generated source for class Ship """
+    name = str()
+
+    #  Team TheQuintet, pair 1 was here. 
+    #  Team Quintent, pair 2 was here
+    #  TODO: create appropriate getter and setter methods
+    #  TODO: Understand encapsulation
+    #  TODO: Understand what these todo comments mean
+    def show(self):
+        """ generated source for method show """
+        #  dunno why this is here maybe it is just an example method
+        print "IF you can't see this then something is severely wrong!!"
+
