8ea860562c9e74a51eadec5d766978dba2b23dc3
reycronin <reykokimonster@gmail.com>
2021-02-12 18:44:28 -0700

    initial tests

---
+++ tests/main.py
@@ -0,0 +1,12 @@
+from Battleship_Tests import *
+
+
+def battleship_test():
+    ships = ['Minesweeper, Destroyer, Battleship']
+    for shipname in ships:
+        Battleship_Tests.ship_length(ships)
+
+
+if __name__== "__main__":
+
+    battleship_tests()
